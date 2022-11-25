3810a491faf8cc65875436adc6e689b5067d7758
patelpalak2300 <119028479+patelpalak2300@users.noreply.github.com>
2022-11-25 10:34:12 +0530

    Add files via upload

---
+++ practice/vk (1).py
@@ -0,0 +1,1374 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import matplotlib
+2+9
+
+
+# In[14]:
+
+
+a = 5.6
+b = 4
+c = a*b
+print(type(c))
+
+
+# In[19]:
+
+
+a = 5
+b = "virat kohli"
+c = a*b
+print(type(c))
+print(c)
+
+
+# In[ ]:
+
+
+
+
+
+# In[29]:
+
+
+a = "virat"
+b = "virat kohli"
+c = b/a
+print(type(c))
+print(c)
+
+
+# In[31]:
+
+
+A1=10
+a1=5
+print(A)
+print(a)
+
+
+# In[32]:
+
+
+A1=10
+a1=5
+print(A)
+print(a)
+
+
+# In[40]:
+
+
+__a = 5
+__a = 100
+
+print(hex(__a))
+
+
+# In[9]:
+
+
+a = 5874/534
+
+print(a)
+
+
+# In[14]:
+
+
+#write a program to find length of hypoteneous of a right angled triangle
+import math
+a = 10
+b = 10
+
+ans = math.sqrt(a**2+ b**2)
+
+print(ans)
+
+
+# In[21]:
+
+
+#wap to find area of circle
+
+from math import *
+
+r = 10
+
+area  = pi*r*r
+
+print(sin 30)
+
+
+# In[41]:
+
+
+a = 0
+b = 3
+not a
+print(bool(not a))
+a or b
+
+
+# In[52]:
+
+
+a = int(input())
+if a<=10
+print("child")
+elif a>10
+print("teenageer")
+else
+print("adult")
+
+
+# In[61]:
+
+
+a = 10
+b = a + 10
+a += 10
+
+print(a)
+
+
+# In[1]:
+
+
+#wap to swap two letters
+
+a = 5
+b = 4
+
+a = a + b
+b = a-b
+a = a-b
+
+print(a)
+print(b)
+
+
+# In[10]:
+
+
+a = 'Abc'
+b = 'AbC'
+b>a
+
+
+# In[16]:
+
+
+7+8
+_ + 8
+_+_
+
+
+# In[17]:
+
+
+_ + 8
+
+
+# In[23]:
+
+
+a = 'Hello'
+'e' in a
+
+
+# In[24]:
+
+
+(20+3)/5*4
+
+
+# In[29]:
+
+
+#wap to find surface area of cylinder and its vol 
+from math import*
+
+r = int(input())
+h = int(input())
+
+area = pi*r*r*h
+
+print(f'vol of cyl is {area : 0.3f}')
+
+
+# In[ ]:
+
+
+a = input()
+b = input()
+
+# int(a)*int(b)
+
+a + b
+
+
+# In[ ]:
+
+
+#wap to get an int value from user which gives output in the form of bin oct and hexa
+
+a  = int(input())
+
+print(f'binary{bin(a)})
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[1]:
+
+
+#wap to get an int value from user which gives output in the form of bin oct and hexa
+
+a  = int(input())
+
+print(f'binary {bin(a)}')
+print(f'hexa {hex(a)}')
+print(f'octa {oct(a)}')
+
+
+# In[24]:
+
+
+#wap to give the folloeing output
+
+q = int(input('enter q :'))
+
+
+
+value = int(input())
+
+discount = int(input())
+
+tax = int(input())
+
+
+amount = (q * value) - discount/100
+
+final_amount = amount + (tax/100)
+
+print('**********')
+print(format('BILL','^10'))
+print('\n')
+print(f'TOTAL QUANTITY {q}')
+print(f'TOTAL VALUE {value * q}$')
+print(f'DISCOUNT {discount}%')
+print(f'AMOUNT {amount}$')
+print(f'TAX {tax}%')
+print(f'FINAL AMOUNT = {final_amount}$')
+      
+      
+
+
+# In[25]:
+
+
+100000/283
+
+
+# In[ ]:
+
+
+
+
+
+# In[6]:
+
+
+#wap to find sq root of a complex number
+import cmath
+a = int(input())
+i = int(input())
+
+b = complex(a,i)
+
+root  = cmath.sqrt(b) 
+print(root)
+
+
+# In[10]:
+
+
+#wap to convert to c to f
+c = int(input())
+
+c = (f-32)*(5/9)
+
+print(c)
+
+
+# In[11]:
+
+
+print(*"python")
+
+
+# In[12]:
+
+
+print(r"c:\some\name")
+
+
+# In[18]:
+
+
+print('''abc      def      gh''')
+
+
+# In[23]:
+
+
+#Wap to find total amount collected in the piggy bank if there are rupees 10 5 2 1 coins in it
+
+rupee1 = int(input("Enter number of 1 rupee coins"))
+rupee2 = int(input("Enter number of 2 rupee coins"))
+rupee5 = int(input("Enter number of 5 rupee coins"))
+rupee10 = int(input("Enter number of 10 rupee coins"))
+
+total = (rupee1 * 1) + (rupee2 * 2) + (rupee5 * 5) + (rupee10 * 10)
+
+print(total)
+
+
+# In[24]:
+
+
++
+
+
+# In[3]:
+
+
+#wap to find average to two numbers and its devation
+
+a = int(input())
+b = int(input())
+
+print(f"Average of two numbers is{(a+b)/2} ")
+
+dev_a = (a-avg)
+dev_b = (b-avg)
+
+
+# In[ ]:
+
+
+
+
+
+# In[5]:
+
+
+a = int(input(Enter a : ))
+b = int(input(Enter b : ))
+
+
+# In[6]:
+
+
++
+
+
+# In[20]:
+
+
+import math
+import cmath
+
+a = int(input("Enter a : "))
+b = int(input("Enter b : "))
+c = int(input("Enter c : "))
+
+m = (b**2-4*a*c)
+
+if(m>=0):
+    print('akshg')
+    d = math.sqrt(m)
+    r1 = (-b+d)/2*a
+    r2 = (-b-d)/2*a
+    print("The roots of equation are : ", r1,r2)
+
+else:
+    d = cmath.sqrt(m)
+    r1 = (-b+d)/2*a
+    r2 = (-b-d)/2*a
+    print("The roots of equation are :  ",r1,r2)    
+    
+
+
+# In[6]:
+
+
+#wap  to get number of days of the user which gives output in the form of years months and days
+
+# days  = input(int("Enter number of days : "))
+                                      
+
+sec  = int(input("Enter number of seconds : "))
+
+hrs = (sec//3600)
+min = (sec - (3600*hrs))//60
+sec=(sec-(3600*hrs + 60*min))
+
+
+print(hrs)
+print(min)
+print(sec)
+
+
+# In[ ]:
+
+
+a = int(input("Enter Roll Number : "))
+b = input("Enter name : ")
+c = bool(input('Belongs to branch CSE[True or False]'))
+
+
+# In[1]:
+
+
+a = input("Enter something : ")
+print(type(a))
+
+
+# In[2]:
+
+
+a = eval(input("Enter something : "))
+print(type(a))
+
+
+# In[ ]:
+
+
+a = eval(input("Enter something : "))
+print(type(a))
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[1]:
+
+
+a = eval(input("Enter something : "))
+print(type(a))
+
+
+# In[3]:
+
+
+a,b = int(input())
+
+
+# In[12]:
+
+
+#wap to convert to degree to radian and vice-versa
+import math
+d = int(input())
+r= int(input())
+
+radians = math.pi*d/180
+
+
+# In[13]:
+
+
+# import math
+a = int(input())
+
+if(a%5 == 0):
+    print("yes")
+else:
+    print("no")
+    
+if(a>5):
+    
+ print("greater than 5")
+
+elif(a == 5):print("Equal to 5")
+else:print("lesser than 5")
+
+
+# In[ ]:
+
+
+
+
+
+# In[19]:
+
+
+#wap to get the total score of indian team if the runrate is > than 8 the output should be 
+total_score = int(input("Enter total runs scored : "))
+
+run_rate = total_score/20
+
+if (run_rate > 8):print(f"\nHigh chances to win as runrate is : {run_rate} ")
+elif (run_rate >7 and run_rate<8):print(f"\nMedium chances to win as runrate is : {run_rate} ")   
+else : print(f"\nLow chances to win as runrate is : {run_rate} ")    
+
+
+# In[24]:
+
+
+#wap to get input from user between 1 and 7
+
+a = int(input("Enter number between 1 to 7 : "))
+
+if(a == 1):print("MONDAY")
+    
+elif(a == 2):print("TUESDAY") 
+    
+elif(a == 3):print("WEDNESDAY")
+
+elif(a == 4):print("THURSDAY") 
+
+elif(a == 5):print("FRIDAY")
+
+elif(a == 6):print("SATURDAY")
+
+elif(a == 7):print("SUNDAY")
+
+else:print("Enter valid number")
+
+
+# In[26]:
+
+
+#WAP TO GENERATE AN ELECTRICITY BILL
+
+units = int(input())
+
+if(units < 100): total_cost = units*0
+    
+elif(units > 100):total_cost = (units-100)*10
+    
+else:total_cost = 100*10 +(units - 200)*15
+    
+print("\n\n********************\n")
+print(f"Total units = {units}")
+print(f"Total amount due = {total_cost}")      
+
+
+# In[10]:
+
+
+price = int(input())
+
+if(price < 80000) : tax = price*0.07
+
+elif(price > 80000 and price<150000) : tax = price*0.15
+    
+else: tax = price*0.18  
+    
+print(f"Ex showroom price : {price}")
+print(f"Tax = {tax}")
+print(f"Total amount payable : {price + tax}")
+
+
+# In[56]:
+
+
+#wap to enter any character or digit from user which gives output in the form of weather entered chsracter is upper case or lower case else its digit
+
+a  = eval(input())
+
+
+if(type(a) == "char") =print("Lower case")
+
+
+    
+elif(a>=97 and a<124): print("Lower case")
+    
+else: print("Digit")
+
+
+# In[1]:
+
+
+a = int(input())
+
+if a>18 :print("eligible")
+else: print(f"{18 - a} Years left")    
+
+
+# In[ ]:
+
+
+ps = int(input("PS = "))
+de = int(input("DE = "))
+fsd = int(input("FSD = "))
+py = int(input("PY = "))
+etc = int(input("ETC = "))
+
+avg = (ps+de+fsd+py+etc)/5
+
+per = ((ps+de+fsd+py+etc)/500)*100
+
+if per>= 80 : print("FIRST CLASS")
+    
+elif per >=70 and per<80 : print("SECOND CLASS")
+    
+else :print("PASS")
+    
+print(f"Average marks obtained = {avg}")
+print(f"Percentage obtained = {per}")
+
+
+# In[1]:
+
+
+salary = int(input())
+gender = eval(input())
+if salary<=10000 : extra_bonus  = salary * 0.02
+elif salary >10000 : extra_bonus = 0    
+
+if gender == "male" : 
+    total = salary + extra_bonus + salary*0.05 
+    bonus = salary*0.05
+else : 
+    total = salary + extra_bonus + salary*0.10 
+    bonus = salary*0.10
+    
+print(f"Total bonus = {extra_bonus + bonus}")   
+print(f"Total payable = {total}")
+
+
+# In[3]:
+
+
+# a = float(input())
+# b = float(input())
+
+while(1>0) :
+    print("Enter the task you want to perform")
+    print("1. +")
+    print("2. -")
+    print("3. /")
+    print("4. *")
+    print("5. %")
+    print("6. //")
+    choice = input()
+    a = float(input("Enter a : "))
+    b = float(input("Enter b : "))
+    if choice == "+" :
+        ans = a+b
+    elif choice =="-" :
+        ans = a-b
+    elif choice == "/" :
+        ans = a/b
+    elif choice == "*" :
+        ans = a*b
+    elif choice == "%" :
+        ans = a % b
+    elif choice == "//" :
+        ans = a // b    
+    else : print("not valid task") 
+        
+    print(ans)    
+        
+
+
+# In[4]:
+
+
+#wap to get a character from user and define weather it is a vowel or a consonent
+inp = input()
+
+if(inp == "a" or inp=="e" or inp == "i" or inp == "o" or inp == "u" or inp == "A" or inp=="U" or inp == "E" or inp == "I" or inp == "O") :
+    print("VOVEL")
+else :
+    print("CONS")
+
+
+# In[4]:
+
+
+inp = input()
+
+an = "AEIOUaeiou"
+
+if inp in an : 
+    print("VOVEL")
+else :
+    print("CONS")
+
+
+# In[23]:
+
+
+#wap to find an year entered is leap or not
+
+a = int(input())
+
+if(a%100 != 0):
+    if(a%4==0):
+        print("LEAP")
+    else:
+        print("not leap")
+else : 
+    if(a%400 == 0):
+        print("leap year")
+    else :
+        print("not leap year")
+                                                                                                                             
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+import calendar
+a = int(input())
+if( calendar.isleap(a)):print("leap")
+else:print("not leap")
+
+
+# In[10]:
+
+
+#for loop
+
+for a in range(0,5,2):
+    print(a)    
+
+
+# In[20]:
+
+
+#wap to get 2 numbers from user which shows starting and ending number and print all the numbers div by three
+
+start = int(input())
+end = int(input())
+div = int(input())
+count = 0
+for a in range(start,end+1):
+    if(a % div == 0):
+        print(a)
+        count += 1
+print(f"count = {count}")        
+
+
+# In[ ]:
+
+
+
+
+
+# In[22]:
+
+
+#wap to get an integer value from user and print its multi table
+
+ab = int(input())
+
+for a in range(1,11):
+    print(f"{ab} x {a} = {ab*a} ")
+
+
+# In[29]:
+
+
+#wap to get an int value from user which gives of all naturals numbers up to that number
+
+a = int(input())
+sum = 1
+for b in range(1,a+1):
+    sum *= b
+print(sum)    
+
+
+# In[5]:
+
+
+#wap to get a user defined range which prints all values except div by 3 and 6
+s = int(input())
+e = int(input())
+c = 0
+for a in range (s,e):
+    if(a%3 != 0):
+        print(a)
+        c += 1
+print(c)        
+
+
+# In[6]:
+
+
+50-16
+
+
+# In[7]:
+
+
+#wap to get 3 num from user which prints the max o these 3
+
+a = int(input())
+b = int(input())
+c = int(input())
+
+if(a>b):
+    if(b>c):
+        print(a)
+    elif(c>a):
+        print(c)
+else        
+        
+
+    
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+#wap to get an int from user which prints t/10
+print(sum)
+
+
+# In[11]:
+
+
+#wap to get 3 numbers from user and print them in ascending order
+
+a = int(input())
+b= int(input())
+c= int(input())
+
+if(a<b):
+    a = a
+else:
+    temp = a
+    a = b
+    b = temp
+if(b<c):
+    b = b
+else:
+    temp = b
+    b = c
+    c = temp
+    
+if(a<b):
+    a = a
+else:
+    temp = a
+    a = b
+    b = temp
+if(b<c):
+    b = b
+else:
+    temp = b
+    b = c
+    c = temp
+    
+print(c)
+print(b)
+print(a)
+
+
+# In[18]:
+
+
+a = 0
+b = 6
+
+x = (a or b) or((a and a) or (a and b))
+y = not(x)
+print(x)
+print(y)
+
+
+# In[7]:
+
+
+a = int(input())
+count = 0
+d = a
+while d>0:
+    count += 1
+    d = d//10
+    
+    
+if(count == 3):
+    b = a//10
+    c = b%10
+    print(c)
+    
+
+
+# In[8]:
+
+
+123//10
+12%10
+
+
+# In[13]:
+
+
+#wap to print fibbonaci series upto n th number
+
+a = 0
+b = 1
+
+till = int(input())
+
+print(a)
+print(b)
+
+for n in range(2,till):
+    s = a+b
+    print(a)
+    a = b
+    b = s
+
+
+# In[18]:
+
+
+123%10
+123//10
+count += 1
+
+
+# In[8]:
+
+
+a = int(input())
+
+s = 0
+
+while(a>0):
+    d = a%10
+    s = s*10+d
+    a = a//10
+    
+print(s)    
+
+
+# In[13]:
+
+
+#palindrome
+a = int(input())
+
+s = 0
+
+while(a>0):
+    d = a%10
+    s = s*10+d
+    a = a//10
+    
+print(s)
+if (a==s+):
+    print("palindrome")
+  
+
+
+# ### wap to get 10 int inputs from user in a loop and print the sum and its avg
+# 
+
+# In[3]:
+
+
+sum = 0
+count = 0
+
+while(True):
+        a = int(input())
+        if(a==-1):
+            break
+        else:
+             sum = sum + a
+             count += 1
+   
+print(f"sum = {sum}")
+print(f"avg = {sum//(count)}")
+
+
+# In[9]:
+
+
+#wap to get initial value of investment and rate of intrest from user and also years
+
+i_value = float(input("initial value : "))
+n_years = int(input("number of years : "))
+rate = float(input("Rate : "))
+
+for n in range(1,n_years+1):
+    intr = (i_value * rate)/100
+    total = intr+i_value
+    print(f"total after year {n} = {total}")
+    i_value = total
+
+
+# In[1]:
+
+
+if 4+5 == 10:
+    print("TRUE")
+else:
+    print("FALSE")
+print("TRUE")
+
+
+# In[5]:
+
+
+a = int(input())
+
+flag = 0
+
+for i in range(a-1,2,-1):
+    if(a%i == 0):
+        flag = 1
+        break
+if(flag == 1):print("not prime")
+else:print("prime")
+
+
+# In[32]:
+
+
+start = int(input())
+end = int(input())
+
+flag = 0
+for i in range(start,end+1):
+ if i>1:
+    for j in range(2,i):
+        if(i%j == 0):
+            break
+    else:
+            print(i)
+            
+
+
+# In[ ]:
+
+
+a = int(input())
+
+sum = 1
+
+for i in range(1,a+1):
+    sum = sum*i
+    
+print(sum)
+
+temp = sum
+count = 0
+while(temp>0):
+    x =temp%10
+    if(x==0):
+        count += 1
+    temp = temp/10
+print(count)
+    
+
+
+# In[ ]:
+
+
+import math
+
+a += math.pi
+
+print(a)
+
+
+# In[21]:
+
+
+#wap to get nth value from user which should print the following sequence and should show its summation
+
+end = eval(input())
+sum = 0
+a = 1
+while(a<end+1):
+    sum = sum+(1/a)
+    a +=1
+print(sum)
+
+while(a<end+1):
+    print('1'+'/')
+    a +=1
+
+
+# In[2]:
+
+
+#wap to print values from 1 to 50 if there are mutiples of three it shoud print fizz if multiple of 5 print buzz
+
+for i in range(1,51):
+    if(i%3 == 0 and i%5 == 0):print("FizzBuzz")
+    elif(i%3 == 0): print("Fizz")
+    elif(i%5 == 0): print("Buzz")
+    else:print(i)
+
+
+# In[11]:
+
+
+for i in range(1,7):
+    for j in range(1,i):
+        print("*",end =" ")
+    print()
+
+
+# In[23]:
+
+
+for i in range(6,1,-1):
+    for j in range(1,i):
+        print("*",end=" ")
+    print()    
+    
+
+
+# In[45]:
+
+
+for i in range(5):
+    for j in range(5-i):
+        print(" ",end="")
+    for k in range(i+1):
+            print("*",end=" ")
+    print()  
+
+
+# In[49]:
+
+
+for i in range(5):
+    for j in range(5-i):
+        print(" ",end=" ")
+    for k in range(i+1):
+            print("*",end=" ")
+    print()  
+
+
+# In[68]:
+
+
+for i in range(5,0,-1):
+    for j in range(6-i):
+        print(" ",end = " ")
+    for j in range(i):
+        print("*",end = " ")
+    print()
+
+
+# In[71]:
+
+
+for i in range(6):
+    for j in range(i):
+        print(i,end=" ")
+    print()
+
+
+# In[74]:
+
+
+for i in range(6):
+    for j in range(1,i):
+        print(j,end=" ")
+    print()
+
+
+# In[78]:
+
+
+for i in range(6,0,-1):
+    for j in range(1,i):
+        print(j,end=" ")
+    print()
+
+
+# In[81]:
+
+
+for i in range(1,6):
+    for j in range(i):
+        if i%2 == 0 : print("#",end=" ")
+        else : print("*",end=" ")
+    print()
+
+
+# In[84]:
+
+
+for i in range(1,6):
+     for k in range(5-i):
+         print(" ",end="")
+     for j in range(i):
+         if i%2 == 0 : print("#",end=" ")
+         else : print("*",end=" ")
+     print()
+
+
+# In[86]:
+
+
+print(‘hello’, ‘how’, ‘are’,‘you’ + ‘-‘ * 4)
+
+
+# In[91]:
+
+
+for num in range(10, 14):
+ for i in range(2, num):
+       if num%i == 1:
+           print(num)
+           break
+
+
+# In[ ]:
+
+
+#armstrog
+
+a = 153
+temp = a
+f = a
+s = 0
+count = 0
+while(a>0):
+    x = a%10
+    count += 1
+    a//10
+k = count
+print(k)
+# while(count!=0):
+#     y = temp%10
+#     s = s+(y**k)
+#     temp//10
+# if f == s:print("arm")
+# else : print("na")
+
+
+# In[ ]:
+
+
+#armstrog
+
+a = 153
+temp = a
+f = a
+s = 0
+count = 0
+while(a>0):
+    x = a%10
+    count += 1
+    a//10
+k = count
+print(k)
+# while(count!=0):
+#     y = temp%10
+#     s = s+(y**k)
+#     temp//10
+# if f == s:print("arm")
+# else : print("na")
+
